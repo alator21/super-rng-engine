8930e9df34cc9be4d829cc84387680c72d9f2dba
alator21 <botazz133@gmail.com>
2025-02-22 21:31:57 +0200

    Improve engine creation

--- src/RNGFactory.ts
+++ src/RNGFactory.ts
@@ -4,14 +4,18 @@ import type { RngEngine } from "./engine/RngEngine";
 import { XORShift128PlusEngine } from "./engine/XORShift128PlusEngine";
 
 export class RNGFactory {
-  static createEngine(type: "mulberry32" | "xorshift128plus" | "mersenne-twisster", seed: number, state?: string): RngEngine {
+  static createEngine(type: "mulberry32" | "xorshift128plus" | "mersenne-twister", opts: { seed: number } | { state: string }): RngEngine {
+    const seed: number = 'seed' in opts ? opts.seed : 100;
+    let state: string | undefined = 'state' in opts ? opts.state : undefined;
+
+
     let engine: RngEngine;
     if (type === "mulberry32") engine = new Mulberry32Engine(seed);
     else if (type === "xorshift128plus") engine = new XORShift128PlusEngine(seed);
-    else if (type === "mersenne-twisster") engine = new MersenneTwisterEngine(seed);
+    else if (type === "mersenne-twister") engine = new MersenneTwisterEngine(seed);
     else throw new Error("Unsupported RNG type");
 
-    if (state) engine.setState(state);
+    if (state !== undefined) engine.setState(state);
     return engine;
   }
 }
